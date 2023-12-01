325d5659f591591f6fae9df59f1b4bda00d1dd95
DtnTrinh <117422446+DtnTrinh@users.noreply.github.com>
2023-12-01 10:05:14 +0700

    Update TaiKhoanDTO.cs

--- QuanLyCuaHangDienThoai/QuanLyCuaHangDienThoai/DTO/TaiKhoanDTO.cs
+++ QuanLyCuaHangDienThoai/QuanLyCuaHangDienThoai/DTO/TaiKhoanDTO.cs
@@ -13,5 +13,19 @@ namespace QuanLyCuaHangDienThoai.DTO
         private string username;
         private string password;
         private string tinhTrang;
-    }
+        public TaiKhoanDTO(string maTK, string maNV, string username, string password, string tinhTrang)
+        {
+            this.maTK = maTK;
+            this.maNV = maNV;
+            this.username = username;
+            this.password = password;
+            this.tinhTrang = tinhTrang;
+        }
+
+        public string MaTK { get => maTK; set => maTK = value; }
+        public string MaNV { get => maNV; set => maNV = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string TinhTrang { get => tinhTrang; set => tinhTrang = value; }
+    } 
 }
