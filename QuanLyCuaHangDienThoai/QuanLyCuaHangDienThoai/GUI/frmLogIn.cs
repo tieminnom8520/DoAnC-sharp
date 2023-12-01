b9c39f1511a9fb8675a1b2f3d07fcfc689b452a3
DtnTrinh <117422446+DtnTrinh@users.noreply.github.com>
2023-12-01 10:12:02 +0700

    upload lai form login

---
+++ QuanLyCuaHangDienThoai/QuanLyCuaHangDienThoai/GUI/frmLogIn.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyCuaHangDienThoai.BUS;
+using QuanLyCuaHangDienThoai.DTO;
+
+namespace QuanLyCuaHangDienThoai.GUI
+{
+    public partial class frmLogIn : Form
+    {
+        public frmLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void picShow_Click(object sender, EventArgs e)
+        {
+
+            if (picShow.Visible == true)
+            {
+                txtPass.UseSystemPasswordChar = false;
+                picShow.Visible = false;
+                picHide.Visible = true;
+            }
+        }
+
+        private void picHide_Click(object sender, EventArgs e)
+        {
+            if (picHide.Visible == true)
+            {
+                txtPass.UseSystemPasswordChar = true;
+                picShow.Visible = true;
+                picHide.Visible = false;
+            }
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPass.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                TaiKhoan_BUS taiKhoan = new TaiKhoan_BUS();
+                DataTable result = taiKhoan.CheckUser(username, password);
+                
+                if (result.Rows.Count > 0)
+                {
+                    this.Hide();
+                    UI ui = new UI();
+                    ui.name = txtUserName.Text;
+                    ui.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("No account available with this username and password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value in all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
