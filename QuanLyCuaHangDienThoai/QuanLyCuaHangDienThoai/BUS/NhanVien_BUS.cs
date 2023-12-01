61dd26a34b628530e4b7948924fb91b8bca940c8
DtnTrinh <117422446+DtnTrinh@users.noreply.github.com>
2023-12-01 10:08:49 +0700

    Upload nhanvien + taikhoan

---
+++ QuanLyCuaHangDienThoai/QuanLyCuaHangDienThoai/BUS/NhanVien_BUS.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace QuanLyCuaHangDienThoai.BUS
+{
+    internal class NhanVien_BUS
+    {
+        Database db;
+        public NhanVien_BUS()
+        {
+            db = new Database();
+        }
+        public DataTable layDanhSachTenNhanVien()
+        {
+            string sql = "Select TENNV from NHANVIEN";
+            return db.Execute(sql);
+        }
+        public DataTable layDanhSachNhanVien()
+        {
+            string sql = "Select * from NHANVIEN";
+            return db.Execute(sql);
+        }
+        public DataRow layThongTinNhanVien(string tenNV)
+        {
+            // Lưu ý: Bạn cần thay đổi câu truy vấn và thực hiện kết nối cơ sở dữ liệu tương ứng
+            string sql = $"SELECT * FROM NHANVIEN WHERE TENNV = N'{tenNV}'";
+
+            DataTable dt = db.Execute(sql);
+
+            // Kiểm tra nếu có dữ liệu trả về
+            if (dt.Rows.Count > 0)
+            {
+                return dt.Rows[0];
+            }
+
+            // Trả về null nếu không tìm thấy nhân viên
+            return null;
+        }
+
+        // Cập nhật thông tin của một nhân viên
+        public void capNhatNhanVien(string tenNV, string sdt, string email, string diaChi, string chucVu)
+        {
+            // Lưu ý: Bạn cần thay đổi câu truy vấn và thực hiện kết nối cơ sở dữ liệu tương ứng
+            string sql = String.Format("update TAIKHOAN set SDT = '{0}',  EMAIL = '{1}', DIACHI = N'{2}', CHUCVU = N'{3}' WHERE TENNV = N'{4}'", sdt, email, diaChi, chucVu, tenNV);
+            db.ExecuteNonQuery(sql);
+        }
+
+
+        // Thêm một nhân viên mới
+        public void themNhanVien(string tenNV, string sdt, string email, string diaChi, string chucVu)
+        {
+            string sql = String.Format("insert into NHANVIEN (TENNV, SDT, EMAIL, DIACHI, CHUCVU) values(N'{0}', '{1}','{2}',N'{3}',N'{4}')", tenNV, sdt, email, diaChi, chucVu );
+
+            db.ExecuteNonQuery(sql);
+        }
+        public void xoaNhanVien(string ma)
+        {
+            string sql = String.Format("delete from NHANVIEN where MANV = {0}", Int32.Parse(ma));
+            db.ExecuteNonQuery(sql);
+        }
+    }
+}
---
+++ QuanLyCuaHangDienThoai/QuanLyCuaHangDienThoai/BUS/TaiKhoan_BUS.cs
@@ -0,0 +1,49 @@
+﻿using QuanLyCuaHangDienThoai.BUS;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangDienThoai.BUS
+{
+    internal class TaiKhoan_BUS
+    {
+        Database db;
+        public TaiKhoan_BUS() 
+        {
+            db = new Database();
+        }
+        public DataTable CheckUser(string username, string password)
+        {
+            string sql = $"SELECT * FROM TAIKHOAN WHERE USERNAME = '{username}' AND PASSWORD = '{password}'";
+            return db.Execute(sql);
+        }
+        public DataTable layDanhSachTaiKhoan()
+        {
+            string sql = "Select * from TAIKHOAN";
+            return db.Execute(sql);
+        }
+        public void themTaiKhoan(int manv,string username, string password,int tinhtrang)
+        {
+            string sql = String.Format("insert into TAIKHOAN(MANV, USERNAME,PASSWORD,TINHTRANG) values('{0}', '{1}','{2}','{3}')", manv, username, password, tinhtrang);
+            db.ExecuteNonQuery(sql);
+        }
+        public void suaTaiKhoan(string ma, int manv, string username, string password, int tinhtrang)
+        {
+            string sql = String.Format("update TAIKHOAN set MANV = N'{0}', USERNAME = {1}, PASSWORD = {2}, TINHTRANG = {3} where MATK = {4}", manv, username, password,tinhtrang, Int32.Parse(ma));
+            db.ExecuteNonQuery(sql);
+        }
+        public void xoaTaiKhoan(string ma)
+        {
+            string sql = String.Format("delete from TAIKHOAN where MATK = {0}", Int32.Parse(ma));
+            db.ExecuteNonQuery(sql);
+        }
+        public DataTable timKiemTaiKhoan(string username)
+        {
+            string sql = String.Format("select * from TAIKHOAN where USERNAME like N'%{0}%'", username);
+            return db.Execute(sql);
+        }
+    }
+}
